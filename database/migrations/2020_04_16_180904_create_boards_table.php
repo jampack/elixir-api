6852ce63343cc3130c09d3a44ae7738a4346c7ce
Akbar Khan <akbar@erip.me>
2020-04-22 22:05:33 +0500

    Saving for History

---
+++ database/migrations/2020_04_16_180904_create_boards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('board');
+    }
+}
